1aa0bf4a1a6e85ab66bfc6543af831c03abfc2dd
svaldivia1996 <sebastianvaldiviareyes@hotmail.com>
2019-10-19 14:27:55 -0300

    Added getters of the Tactician

--- src/controller/Tactician.java
+++ src/controller/Tactician.java
@@ -16,10 +16,59 @@ import java.util.List;
  * @since 2.0
  */
 public class Tactician {
+
+    private String name;
     private Field map;
     private List<IUnit> units = new ArrayList<>();
     private List<IEquipableItem> items = new ArrayList<>();
     private IUnit selectedUnit;
 
+    /**
+     * Creates a new Tactician
+     * @param name of the tactician
+     * @param map of the game
+     * @param units the units that can handle
+     * @param items the items that can handle
+     */
+    public Tactician (String name , Field map, List<IUnit> units, List<IEquipableItem> items){
+        this.name = name;
+        this.map = map;
+        this.units = units;
+        this.items = items;
+    }
 
+    /**
+     * @return the name of the tactician
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @return the map of the game
+     */
+    public Field getMap() {
+        return map;
+    }
+
+    /**
+     * @return a List with all the tactician units
+     */
+    public List<IUnit> getUnits() {
+        return units;
+    }
+
+    /**
+     * @return a List with all the tactician Items
+     */
+    public List<IEquipableItem> getItems() {
+        return items;
+    }
+
+    /**
+     * @return the selected unit of the tactician
+     */
+    public IUnit getSelectedUnit() {
+        return selectedUnit;
+    }
 }
